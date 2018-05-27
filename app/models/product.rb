0300cb6fd431b6e60ae01d0e257cf2272f2131fc
JackMarx <seriousfools@gmail.com>
2018-05-27 11:35:06 -0500

    authentication

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,12 +1,7 @@
 class Product < ApplicationRecord
-
-  def supplier
-    Supplier.find_by(id: supplier_id)
-  end
-
-  def images
-    Image.where(product_id: self.id)
-  end
+  belongs_to :supplier
+  has_many :images
+  has_many :orders
 
   def is_discounted?
     price < 10
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  post 'user_token' => 'user_token#create'
+  post "/users" => "users#create"
+  
   namespace :api do
     get '/products' => 'products#index'
     get '/products/:id' => 'products#show'
